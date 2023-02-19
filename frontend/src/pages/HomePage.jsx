ea4537f66e6646299336f399dbb0fc52e428c70e
Md-Gulzeesh <gulzeesh2000@gmail.com>
2023-02-19 16:35:08 +0530

    homePage heading added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,13 +3,16 @@ import styles from "./HomePage.module.css";
 import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
-    <div className={styles.container}>
-      <button className={styles.btn}>Fetch Users</button>
-      <button className={styles.btn}>Delete Users</button>
-      <Link className={styles.link} to={"/users"}>
-        <button className={styles.btn}>User Details</button>
-      </Link>
-    </div>
+    <>
+      <h1 className={styles.homePage_heading}>Home Page</h1>
+      <div className={styles.container}>
+        <button className={styles.btn}>Fetch Users</button>
+        <button className={styles.btn}>Delete Users</button>
+        <Link className={styles.link} to={"/users"}>
+          <button className={styles.btn}>User Details</button>
+        </Link>
+      </div>
+    </>
   );
 };
 
