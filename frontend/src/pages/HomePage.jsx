83f8f4750062ac74cb945d7108802effe742ee84
Md-Gulzeesh <gulzeesh2000@gmail.com>
2023-02-19 13:16:24 +0530

    Homepage css added and default css removed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import styles from "./HomePage.module.css";
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className={styles.container}>
+      <button className={styles.btn}>Fetch Users</button>
+      <button className={styles.btn}>Delete Users</button>
+      <button className={styles.btn}>User Details</button>
+    </div>
   )
 }
 
