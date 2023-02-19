d47f519150bf4cecbdc54f035dcf99a83f3b86ea
Md-Gulzeesh <gulzeesh2000@gmail.com>
2023-02-19 13:30:54 +0530

    link and root variable added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 import styles from "./HomePage.module.css";
+import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <div className={styles.container}>
       <button className={styles.btn}>Fetch Users</button>
       <button className={styles.btn}>Delete Users</button>
-      <button className={styles.btn}>User Details</button>
+      <Link className={styles.link} to={"/users"}>
+        <button className={styles.btn}>User Details</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
