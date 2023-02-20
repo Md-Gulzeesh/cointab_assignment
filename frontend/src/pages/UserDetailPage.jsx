e9e0cfffc3b1ad696a2826f4d6c811d3c4bc0616
Md-Gulzeesh <gulzeesh2000@gmail.com>
2023-02-20 12:58:20 +0530

    pagination and loading,error indicators added

--- frontend/src/pages/UserDetailPage.jsx
+++ frontend/src/pages/UserDetailPage.jsx
@@ -4,18 +4,29 @@ import { IoArrowBackOutline } from "react-icons/io5";
 import {MdCopyAll} from "react-icons/md"
 import { Link } from "react-router-dom";
 import Pagination from "../components/Pagination";
+import { BACKEND_API_URL } from "../api/api";
+import Loader from "../components/loader";
 const UserDetailPage = () => {
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
   useEffect(() => {
     getData();
   }, []);
   const getData = async () => {
+    setIsLoading(true);
     try {
-      let response = await fetch("https://randomuser.me/api/?results=50");
+      let response = await fetch(BACKEND_API_URL+"user");
       response = await response.json();
-      setData(response.results);
-      console.log(response.results);
+      setData(response.data);
+      setTotalPages(response.totalPages);
+      console.log(response.data);
+      setIsLoading(false);
     } catch (error) {
+      setIsError(true);
+      setIsLoading(false);
       console.log(error);
     }
   };
@@ -27,61 +38,84 @@ const UserDetailPage = () => {
         </Link>
         <h2 className={styles.user_details_heading}>User Details</h2>
       </div>
-      <table className={styles.customTable}>
-        <thead className={styles.tableHeader}>
-          <tr className={styles.tableRow}>
-            <th className={styles.tableHead}>Avatar</th>
-            <th className={styles.tableHead}>Name</th>
-            <th className={styles.tableHead}>Email</th>
-            <th className={styles.tableHead}>Gender</th>
-            <th className={styles.tableHead}>Address</th>
-            <th className={styles.tableHead}>City</th>
-            <th className={styles.tableHead}>State</th>
-            <th className={styles.tableHead}>Country</th>
-            <th className={styles.tableHead}>Phone</th>
-            <th className={styles.tableHead}>Age</th>
-            <th className={styles.tableHead}>Date of Birth</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data?.map((elem, index) => (
-            <tr key={index} className={styles.tableRow}>
-              <td className={styles.tableData} data-title="Avatar">
-                <img
-                  className={styles.avatar_image}
-                  src={elem?.picture?.large}
-                  alt={elem?.name?.first + " " + elem?.name?.last}
-                />
-              </td>
-              <td className={styles.tableData} data-title="Name">
-                {elem?.name?.title +
-                  " " +
-                  elem?.name?.first +
-                  " " +
-                  elem?.name?.last}
-              </td>
-              <td className={styles.tableData} data-title="Email">
-                <div>
-                  {elem?.email} <MdCopyAll style={{ fontSize: "16px" }} />
-                </div>
-              </td>
-              <td className={styles.tableData} data-title="Gender">{elem?.gender}</td>
-              <td className={styles.tableData} data-title="Address">
-                {elem?.location?.street?.number +
-                  " " +
-                  elem?.location?.street?.name}
-              </td>
-              <td className={styles.tableData} data-title="City">{elem?.location?.city}</td>
-              <td className={styles.tableData} data-title="State">{elem?.location?.state}</td>
-              <td className={styles.tableData} data-title="Country">{elem?.location?.country}</td>
-              <td className={styles.tableData} data-title="Phone">{elem?.phone}</td>
-              <td className={styles.tableData} data-title="Age">{elem?.dob?.age}</td>
-              <td className={styles.tableData} data-title="Date of Birth">{elem?.dob?.date}</td>
+      {isLoading ? (
+        <div className={styles.loaderDiv}>
+          <Loader />
+        </div>
+      ) : isError ? (
+        <div className={styles.loaderDiv} color={"red"}>Something went wrong! see console for more information</div>
+      ) : (
+        <table className={styles.customTable}>
+          <thead className={styles.tableHeader}>
+            <tr className={styles.tableRow}>
+              <th className={styles.tableHead}>Avatar</th>
+              <th className={styles.tableHead}>Name</th>
+              <th className={styles.tableHead}>Email</th>
+              <th className={styles.tableHead}>Gender</th>
+              <th className={styles.tableHead}>Address</th>
+              <th className={styles.tableHead}>City</th>
+              <th className={styles.tableHead}>State</th>
+              <th className={styles.tableHead}>Country</th>
+              <th className={styles.tableHead}>Phone</th>
+              <th className={styles.tableHead}>Age</th>
+              <th className={styles.tableHead}>Date of Birth</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <Pagination current={1}/>
+          </thead>
+          <tbody>
+            {data?.map((elem, index) => (
+              <tr key={index} className={styles.tableRow}>
+                <td className={styles.tableData} data-title="Avatar">
+                  <img
+                    className={styles.avatar_image}
+                    src={elem?.picture?.large}
+                    alt={elem?.name?.first + " " + elem?.name?.last}
+                  />
+                </td>
+                <td className={styles.tableData} data-title="Name">
+                  {elem?.name?.title +
+                    " " +
+                    elem?.name?.first +
+                    " " +
+                    elem?.name?.last}
+                </td>
+                <td className={styles.tableData} data-title="Email">
+                  <div>
+                    {elem?.email} <MdCopyAll style={{ fontSize: "16px" }} />
+                  </div>
+                </td>
+                <td className={styles.tableData} data-title="Gender">
+                  {elem?.gender}
+                </td>
+                <td className={styles.tableData} data-title="Address">
+                  {elem?.location?.street?.number +
+                    " " +
+                    elem?.location?.street?.name}
+                </td>
+                <td className={styles.tableData} data-title="City">
+                  {elem?.location?.city}
+                </td>
+                <td className={styles.tableData} data-title="State">
+                  {elem?.location?.state}
+                </td>
+                <td className={styles.tableData} data-title="Country">
+                  {elem?.location?.country}
+                </td>
+                <td className={styles.tableData} data-title="Phone">
+                  {elem?.phone}
+                </td>
+                <td className={styles.tableData} data-title="Age">
+                  {elem?.dob?.age}
+                </td>
+                <td className={styles.tableData} data-title="Date of Birth">
+                  {elem?.dob?.date}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+
+      <Pagination current={page} totalPages={totalPages} />
     </div>
   );
 };
